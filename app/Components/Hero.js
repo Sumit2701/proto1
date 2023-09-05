26c65383de79284597ca2bf2d59a1700d91957be
Sumit2701 <sumitahire2701@gmail.com>
2023-09-05 20:26:20 +0530

    commit 2

--- app/Components/Hero.js
+++ app/Components/Hero.js
@@ -4,10 +4,10 @@ import Image from 'next/image'
 export default function Hero() {
   return (
     <div className='flex md:flex-row flex-col'>
-        <div className='ss:leading-[100.8px] leading-[75px] ss:text-[68px] text-[52px] sm:px-16 px-6 font-bold  sm:my-10 my-2 items-center'>
-        Simplify  <span className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-pink-600">
-        Photo Selection:
-</span> <br/>  Share Your Photos with Your Client for Effortless Decision-Making
+        <div className='ss:leading-[100.8px] leading-[75px] sm:text-[68px] text-[52px] sm:px-16 px-6 font-bold  sm:my-10 my-2 items-center'>
+        Simplify  <span className=" font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-pink-600">
+        Photo 
+</span> <br/> Selection: Share Albums with client
         </div>
         <div>
             <Image src={hero } className=' w-2/3 sm:my-10 my-2 mx-auto'/>
